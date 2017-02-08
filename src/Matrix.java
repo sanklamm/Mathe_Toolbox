acc9ee0e0a3410c0d8ef72c3050bfcf43ce95c67
Sebastian <sanklamm@gmail.com>
2017-02-08 17:56:01 +0100

    added initial Version of Gauss-Seidel algorithm.

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by s on 12.12.16.
  */
@@ -5,6 +7,7 @@
 public class Matrix {
 
     private static final double EPSILON = 1e-10;
+    public static final int MAX_ITERATIONS = 100;
 
     private int m;
     private int n;
@@ -56,7 +59,7 @@ public class Matrix {
         return dim;
     }
 
-    // create and return a random M-by-N matrix with values between 0 and 1
+    // create and return a random M-by-N matrix with values between 0 and 10
     public static Matrix random(int M, int N) {
         Matrix A = new Matrix(M, N);
         for (int i = 0; i < M; i++) {
@@ -236,5 +239,112 @@ public class Matrix {
         return result;
     }
 
+    // Everything Gauss-Seidel
+
+    public boolean transformToDominant(double[][] M, int r, boolean[] V, int[] R)
+    {
+        int n = M.length;
+        if (r == n) {
+            double[][] T = new double[n][n+1];
+            for (int i = 0; i < R.length; i++) {
+                for (int j = 0; j < n + 1; j++)
+                    T[i][j] = M[R[i]][j];
+            }
+
+            M = T;
+
+            return true;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (V[i]) continue;
+
+            double sum = 0;
+
+            for (int j = 0; j < n; j++)
+                sum += Math.abs(M[i][j]);
+
+            if (2 * Math.abs(M[i][r]) > sum) { // diagonally dominant?
+                V[i] = true;
+                R[r] = i;
+
+                if (transformToDominant(M, r + 1, V, R))
+                    return true;
+
+                V[i] = false;
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Returns true if is possible to transform M(data member) to a diagonally
+     * dominant matrix, false otherwise.
+     */
+    public boolean makeDominant()
+    {
+        double[][] M = this.data;
+        int n = this.n;
+        boolean[] visited = new boolean[n];
+        int[] rows = new int[n];
+
+        Arrays.fill(visited, false);
+
+        return transformToDominant(M, 0, visited, rows);
+    }
+
+
+    /**
+     * Applies GaussSeidel method to find the solution of the system
+     * of linear equations represented in matrix M.
+     * M is a matrix with the following form:
+     * a_11 * x_1 + a_12 * x_2 + ... + a_1n * x_n = b_1
+     * a_21 * x_1 + a_22 * x_2 + ... + a_2n * x_n = b_2
+     * .                 .                  .        .
+     * .                 .                  .        .
+     * .                 .                  .        .
+     * a_n1 * x_n + a_n2 * x_2 + ... + a_nn * x_n = b_n
+     */
+    public void solve()
+    {
+        int iterations = 0;
+        int n = this.n;
+        double[][] M = this.data;
+        double epsilon = 1e-15;
+        double[] X = new double[n]; // Approximations
+        double[] P = new double[n]; // Prev
+        Arrays.fill(X, 0);
+
+        while (true) {
+            for (int i = 0; i < n; i++) {
+                double sum = M[i][n]; // b_n
+
+                for (int j = 0; j < n; j++)
+                    if (j != i)
+                        sum -= M[i][j] * X[j];
+
+                // Update x_i to use in the next row calculation
+                X[i] = 1/M[i][i] * sum;
+            }
+
+            System.out.print("X_" + iterations + " = {");
+            for (int i = 0; i < n; i++)
+                System.out.print(X[i] + " ");
+            System.out.println("}");
+
+            iterations++;
+            if (iterations == 1) continue;
+
+            boolean stop = true;
+            for (int i = 0; i < n && stop; i++)
+                if (Math.abs(X[i] - P[i]) > epsilon)
+                    stop = false;
+
+            if (stop || iterations == MAX_ITERATIONS) break;
+            P = (double[])X.clone();
+        }
+    }
 
 }
